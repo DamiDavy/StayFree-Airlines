bbf4549fc15328587fba300e4dda81c36f481eca
OlPeep <evilmall@mail.ru>
2021-02-16 19:30:14 +0300

    added allowed hosts in settings

--- freedom/settings.py
+++ freedom/settings.py
@@ -30,7 +30,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 #export DJANGO_DEBUG=False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['stayfreeairlines.herokuapp.com']
 
 
 # Application definition
